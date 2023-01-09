34596b35f87974b8413ca637f661c8d5ff46acf9
EllieWalsch <elliewalsch4@gmail.com>
2023-01-09 12:44:06 -0600

    Insert seeds into role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,4 +6,16 @@ VALUES ('John', 'Doe', 1, NULL, 1),
        ('Kunal', 'Singh', 5, NULL, 5),
        ('Malia', 'Brown', 6, 5, 6),
        ('Sarah', 'Lourd', 7, NULL, 7),
-       ('Tom', 'Allen', 8, 7, 8);
+       ('Tom', 'Allen', 8, 7, 8)
+;
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, 'Sales Lead', 100000, 1),
+       (2, 'Salesperson', 80000, 1),
+       (3, 'Lead Engineer', 150000, 2),
+       (4, 'Software Engineer', 120000, 2),
+       (5, 'Account Manager', 160000, 3),
+       (6, 'Accountant', 125000, 3),
+       (7, 'Legal Team Lead', 250000, 4),
+       (8, 'Lawyer', 190000, 4);
+;

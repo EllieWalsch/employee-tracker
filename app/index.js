03db7749f08fa6008abea9d287c58af6dbac7833
EllieWalsch <elliewalsch4@gmail.com>
2023-01-09 14:57:17 -0600

    Inquirer prompt to view departments

--- app/index.js
+++ app/index.js
@@ -1,11 +1,25 @@
 import mysql from "mysql2/promise";
 import config from "./config.js";
 import cTable from "console.table";
+import inquirer from "inquirer";
 
 // Creates connection to the database using the imported config
 const conn = await mysql.createConnection(config.db);
 
-const [departments] = await conn.execute("SELECT * FROM department");
-console.table(departments);
+const mainQuestions = [
+  {
+    type: "list",
+    name: "options",
+    message: "What would you like to do?",
+    choices: ["View all departments", "blabla"],
+  },
+];
+
+const data = await inquirer.prompt(mainQuestions);
+
+if (data.options === "View all departments") {
+  const [departments] = await conn.execute("SELECT * FROM department");
+  console.table(departments);
+}
 
 conn.end();

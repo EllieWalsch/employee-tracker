69b31264962b1a2cc0567511e0af845b77852d4a
EllieWalsch <elliewalsch4@gmail.com>
2023-01-09 15:46:54 -0600

    Inquirer prompt to view roles

--- app/index.js
+++ app/index.js
@@ -11,15 +11,28 @@ const mainQuestions = [
     type: "list",
     name: "options",
     message: "What would you like to do?",
-    choices: ["View all departments", "blabla"],
+    choices: ["View all departments", "View all roles"],
   },
 ];
 
 const data = await inquirer.prompt(mainQuestions);
 
 if (data.options === "View all departments") {
+  viewDepartments();
+} else if (data.options === "View all roles") {
+  viewRoles();
+}
+
+async function viewDepartments() {
   const [departments] = await conn.execute("SELECT * FROM department");
   console.table(departments);
+}
+
+async function viewRoles() {
+  const [roles] = await conn.execute(
+    "SELECT * FROM role JOIN department ON role.department_id = department.id"
+  );
+  console.table(roles);
 }
 
 conn.end();

858af77c6b478d6d6b10509efc52b806bf44a0a6
EllieWalsch <elliewalsch4@gmail.com>
2023-01-09 14:24:14 -0600

    Show departments in console.table

--- app/index.js
+++ app/index.js
@@ -1,5 +1,11 @@
 import mysql from "mysql2/promise";
 import config from "./config.js";
+import cTable from "console.table";
 
 // Creates connection to the database using the imported config
 const conn = await mysql.createConnection(config.db);
+
+const [departments] = await conn.execute("SELECT * FROM department");
+console.table(departments);
+
+conn.end();

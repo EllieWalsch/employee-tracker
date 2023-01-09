e4bad421c2569de14fe4938b17bd949580aaa25e
EllieWalsch <elliewalsch4@gmail.com>
2023-01-09 13:25:44 -0600

    Add foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -3,22 +3,29 @@ CREATE DATABASE HW_team;
 
 USE HW_team;
 
-CREATE TABLE employee (
-  id INT NOT NULL PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE department (
-  id INT NOT NULL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  manager_id INT,
+  role_id INT NOT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,8 @@
-INSERT INTO employee (first_name, last_name, id, manager_id, role_id)
-VALUES ('John', 'Doe', 1, NULL, 1),
-       ('Mike', 'Chan', 2, 1, 2),
-       ('Ashley', 'Rodriguez', 3, NULL, 3),
-       ('Kevin', 'Tupik', 4, 3, 4),
-       ('Kunal', 'Singh', 5, NULL, 5),
-       ('Malia', 'Brown', 6, 5, 6),
-       ('Sarah', 'Lourd', 7, NULL, 7),
-       ('Tom', 'Allen', 8, 7, 8)
+INSERT INTO department (id, name)
+VALUES (1, 'Sales'),
+       (2, 'Engineering'),
+       (3, 'Finance'),
+       (4, 'Legal')
 ;
 
 INSERT INTO role (id, title, salary, department_id)
@@ -20,9 +16,14 @@ VALUES (1, 'Sales Lead', 100000, 1),
        (8, 'Lawyer', 190000, 4)
 ;
 
-INSERT INTO department (id, name)
-VALUES (1, 'Sales'),
-       (2, 'Engineering'),
-       (3, 'Finance'),
-       (4, 'Legal')
+INSERT INTO employee (first_name, last_name, id, manager_id, role_id)
+VALUES ('John', 'Doe', 1, NULL, 1),
+       ('Mike', 'Chan', 2, 1, 2),
+       ('Ashley', 'Rodriguez', 3, NULL, 3),
+       ('Kevin', 'Tupik', 4, 3, 4),
+       ('Kunal', 'Singh', 5, NULL, 5),
+       ('Malia', 'Brown', 6, 5, 6),
+       ('Sarah', 'Lourd', 7, NULL, 7),
+       ('Tom', 'Allen', 8, 7, 8)
 ;
+
